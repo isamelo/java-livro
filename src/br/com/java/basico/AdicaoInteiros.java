a93cff1a742a61dae65d6d5ee4e098b2241ff39d
isamelo <isars.melo@gmail.com>
2021-02-05 19:27:47 -0300

    Livro até página 41

--- src/br/com/java/basico/AdicaoInteiros.java
+++ src/br/com/java/basico/AdicaoInteiros.java
@@ -8,26 +8,31 @@ public class AdicaoInteiros {
 		
 		// programa de adição que insere dois números,então exibe a soma deles
 		// cria um scanner para obter a entrada a partir da janela de comando
+		
 		Scanner input = new Scanner(System.in);
 		
 		int number1;
 		int number2;
 		int sum;
 		
-		System.out.println("Digite o primeiro número: ");
+		System.out.print("Digite o primeiro número: "); 
+		//  Utilizamos o método print aqui para que a entrada do usuário apareça na mesma linha que o prompt
 		
 		number1 = input.nextInt(); //lê o primeiro número fornecido
 		
-		System.out.println("Digite o segundo número: ");
+		System.out.print("Digite o segundo número: ");
 		
 		number2 = input.nextInt(); //lê o segundo número fornecido
 		
 		sum = number1 + number2;
 		
-		System.out.printf("Soma é %d%n", sum); // usando printf para apresentar em d o número digitado
+		System.out.printf("Soma é %d%n", sum); 
+		// usando printf para apresentar em d o número digitado -- a letra d significa “inteiro decimal"
 		
 		System.out.println("Soma é: " + sum); // usando o println
 		
+		System.out.println("Soma é: " + (number1 + number2)); // somando dentro da instrução printf
+
 	}
 
 }
